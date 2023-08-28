5ccc66b37905d0e4ac09785cc0070add49b53d43
kauanburger <kauanburger007@gmail.com>
2023-08-27 22:51:58 -0300

    Update pratica.py

--- pratica.py
+++ pratica.py
@@ -1,13 +1,12 @@
+#Importando o módulo math
 from math import sqrt, ceil
 num = int(input('Digite um número: '))
 raiz = sqrt(num)
 
 print('A raiz de {} é {:.2f}'.format(num, ceil(raiz)))
 
+
+#importando o módulo random para sortear um número aleatório
 import random
 num1 = random.randint(1, 100)
 print(num1)
-
-import emoji 
-
-print(emoji.emojize('Basquete é o meu esporte favorito :basketball:'))
